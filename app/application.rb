d760b43b941b5ad6e78f4b6fb090ad078a71b15a
Marc Mills <mdmills1990@gmail.com>
2018-11-06 22:48:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    if req.path.match(/items/)
     if req.path=="/items/#{Item.new}"
       if @@items.include? Item.new
         resp.write = "#{Item.price}"
